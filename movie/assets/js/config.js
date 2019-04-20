e70440070e52fbb9c6a79852a2d8f1ef54425fe2
Quang Sáng <typidane@gmail.com>
2019-04-20 13:42:08 +0700

    first commit

---
+++ movie/assets/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAPxgBhHZNXSbuHXngjvfus3D-KKewjomQ",
+    authDomain: "j2team-movies-30b6b.firebaseapp.com",
+    databaseURL: "https://j2team-movies-30b6b.firebaseio.com",
+    projectId: "j2team-movies-30b6b",
+    storageBucket: "",
+    messagingSenderId: "264023484248"
+};
+firebase.initializeApp(config);
